ced7af5784d60e2eed9207e8461d8de2335724ec
hayakawa6660 <71759305+hayakawa6660@users.noreply.github.com>
2020-10-08 10:14:37 +0900

    Playerクラスを追加 (#1)

---
+++ Source/Scene/Play/Player/Player.cpp
@@ -0,0 +1,23 @@
+#include "Player.h"
+#include "Library/SceneManager/SceneManager.h"
+
+#include "Source/System/ResourceManager/ResourceManager.h"
+
+Player::Player(SceneBase* _scene):
+	GameObject(_scene)
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Update()
+{
+	int a = 10;
+}
+
+void Player::Draw()
+{
+
+}
